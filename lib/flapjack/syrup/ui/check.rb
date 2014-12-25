fd4b31d0e63c7cc29df69cf0a4ef5633f74be906
Mick Pollard <aussielunix@gmail.com>
2014-12-26 09:01:19 +1100

    add example of calling application methods from the GLI ui dsl

--- lib/flapjack/syrup/ui/check.rb
+++ lib/flapjack/syrup/ui/check.rb
@@ -7,18 +7,21 @@ command :check do |c|
   c.command :update do |update|
     update.action do |global_options,options,args|
       puts "TODO: check update"
+      # Flapjack::Syrup::Check.update(global_options,options,args)
     end
   end
   c.desc 'check status'
   c.command :status do |status|
     status.action do |global_options,options,args|
       puts "TODO: check status"
+      # Flapjack::Syrup::Check.status(global_options,options,args)
     end
   end
   c.desc 'test an check'
   c.command :test do |test|
     test.action do |global_options,options,args|
       puts "TODO: check test"
+      # Flapjack::Syrup::Check.test(global_options,options,args)
     end
   end
 end
