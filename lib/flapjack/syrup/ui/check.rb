42e8ddce61897c8039e56ba51babe0a598376e6d
Mick Pollard <aussielunix@gmail.com>
2014-12-26 12:20:21 +1100

    example of using adding an flag to a sub sub command

--- lib/flapjack/syrup/ui/check.rb
+++ lib/flapjack/syrup/ui/check.rb
@@ -3,8 +3,20 @@ long_desc '''
 check commands- bunch of sub commands for check manipulation
 '''
 command :check do |c|
-  c.desc 'update an check'
+  c.desc 'Modify a check.'
+  c.long_desc '''
+Specify IDs as comma-separated values, or no IDs to update all.
+
+Check IDS are a combination of the entity name and check name, separated by a colon.
+
+Example: [--ids ENTITY:CHECK,ENTITY:CHECK]
+
+WARNING: There is no way to re-enable a check via the API once it has been disabled! Flapjack will re-enable it when a new event is received for it.
+'''
   c.command :update do |update|
+    update.desc "Check identifiers (comma-separated, format '<entity_name>:<check_name>')"
+    update.arg_name 'ids'
+    update.flag [:ids]
     update.action do |global_options,options,args|
       puts "TODO: check update"
       # Flapjack::Syrup::Check.update(global_options,options,args)
